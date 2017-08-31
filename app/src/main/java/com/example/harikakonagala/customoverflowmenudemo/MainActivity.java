119aaa42c1b5210fe3a7a741c2471acc93b2ebdf
phanindrakannaji <hk12@umbc.edu>
2017-08-31 02:14:03 -0400

    Replaced menu with custom dialog fragment

--- app/src/main/java/com/example/harikakonagala/customoverflowmenudemo/MainActivity.java
+++ app/src/main/java/com/example/harikakonagala/customoverflowmenudemo/MainActivity.java
@@ -1,81 +1,45 @@
 package com.example.harikakonagala.customoverflowmenudemo;
 
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.ImageButton;
-import android.widget.LinearLayout;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private ImageButton left, right, bookmark;
+    private ImageButton customMenu;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+        customMenu = (ImageButton) findViewById(R.id.custom_menu);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
-        getMenuInflater().inflate(R.menu.custom_menu, menu);
-        return true;
+    protected void onResume() {
+        super.onResume();
+        customMenu.setOnClickListener(this);
     }
 
     @Override
-    public boolean onPrepareOptionsMenu(Menu menu) {
-
-        final MenuItem overflowItem = menu.findItem(R.id.action_overflow_menu);
-        LinearLayout rootLayout = (LinearLayout) overflowItem.getActionView();
-
-        left = (ImageButton) rootLayout.findViewById(R.id.image_button_back);
-        bookmark = (ImageButton) rootLayout.findViewById(R.id.image_button_bookmark);
-        right = (ImageButton) rootLayout.findViewById(R.id.image_button_forward);
-
-        rootLayout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onOptionsItemSelected(overflowItem);
-            }
-        });
-
-        left.setOnClickListener(this);
-        right.setOnClickListener(this);
-        bookmark.setOnClickListener(this);
-
-        return super.onPrepareOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        switch (item.getItemId()){
-            case R.id.action_overflow_menu:
-                updateOverFlowMenu();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-
-    private void updateOverFlowMenu() {
-        //set onclick listeners for buttons
+    protected void onPause() {
+        super.onPause();
+        customMenu.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
+        Toast mToast = Toast.makeText(getApplicationContext(), "", Toast.LENGTH_SHORT);
         switch(v.getId()){
-            case R.id.image_button_back:
-                Toast.makeText(getApplicationContext(), "Go Back", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.image_button_forward:
-                Toast.makeText(getApplicationContext(), "Go Forward", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.image_button_bookmark:
-                Toast.makeText(getApplicationContext(), "Bookmark Option", Toast.LENGTH_SHORT).show();
+            case R.id.custom_menu:
+                DialogFragment newFragment = MenuDialogFragment.newInstance();
+                newFragment.show(getSupportFragmentManager(), "");
                 break;
         }
     }
---
+++ app/src/main/java/com/example/harikakonagala/customoverflowmenudemo/MenuDialogFragment.java
@@ -0,0 +1,82 @@
+package com.example.harikakonagala.customoverflowmenudemo;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+/**
+ * Created by phani on 8/31/17.
+ */
+
+public class MenuDialogFragment extends DialogFragment implements View.OnClickListener {
+
+    ImageButton left, right, bookmark;
+
+    static MenuDialogFragment newInstance() {
+        return new MenuDialogFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStyle(DialogFragment.STYLE_NO_TITLE, android.R.style.Theme_Holo_Light_Dialog);
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View subView = inflater.inflate(R.layout.activity_custom_menu, null);
+
+        left = (ImageButton) subView.findViewById(R.id.image_button_back);
+        right = (ImageButton) subView.findViewById(R.id.image_button_forward);
+        bookmark = (ImageButton) subView.findViewById(R.id.image_button_bookmark);
+
+        left.setOnClickListener(this);
+        right.setOnClickListener(this);
+        bookmark.setOnClickListener(this);
+
+        Window window = getDialog().getWindow();
+        window.setGravity(Gravity.END | Gravity.TOP);
+        WindowManager.LayoutParams p = getDialog().getWindow().getAttributes();
+        p.width = 70;
+        p.x = 10;
+        p.y = 100;
+        window.setAttributes(p);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+
+        return subView;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast mToast = Toast.makeText(getActivity().getApplicationContext(), "", Toast.LENGTH_SHORT);
+        switch (v.getId()){
+            case R.id.image_button_back:
+                mToast.setText("Back");
+                mToast.show();
+                break;
+            case R.id.image_button_forward:
+                mToast.setText("Forward");
+                mToast.show();
+                break;
+            case R.id.image_button_bookmark:
+                mToast.setText("Bookmark");
+                mToast.show();
+                break;
+        }
+    }
+}
